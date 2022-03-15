bf3ba4c4f394a64269997b6281384e0c5a7431e9
vgalinskii <vgalinskii@turbosolution.ru>
2022-03-15 10:03:33 +0300

    на работе

---
+++ Indi_egoroff_channel/strings_and_lists.py
@@ -0,0 +1,24 @@
+# 2.4 Комментарии в коде
+
+# 2.3 Методы строк
+# '''
+# Sample Input:
+# Codeforces
+# Sample Output:
+# .c.d.f.r.c.s
+# '''
+# def replace_char(symbol:str)->str:
+#     if symbol in 'AOYEUI':
+#         return ''
+#     else:
+#         return '.' + symbol.lower()
+
+# s = "Codeforces".upper() #input().upper()
+# res = ''
+# for ch in s:
+#     res = res + replace_char(ch)
+# print(res)    
+
+# 2.2 Строки: индексы и срезы
+
+# 2.1 Cтроки и операции над ними
