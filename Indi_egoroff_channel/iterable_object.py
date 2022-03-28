8f4f3cb38a64d8e3731612ddf4ccec307fa4eb60
vgalinskii <vgalinskii@turbosolution.ru>
2022-03-28 10:00:02 +0300

    доработано на работе.

--- Indi_egoroff_channel/iterable_object.py
+++ Indi_egoroff_channel/iterable_object.py
@@ -13,7 +13,7 @@
 # 4
 # in_str = input()
 # in_count = int(input())
-# [ print(in_str) for _ in range(in_count)]
+# [print(in_str) for _ in range(in_count)]
 # 5
 # for value in range(int(input()), int(input()) + 1):
 #     if value % 3 == 0 and value % 5 == 0:
@@ -45,4 +45,33 @@
 #     if "рок" in word:
 #         print(my_list.index(word) + 1, word.find("рок") + 1)
 # print(my_list)
+
+#  9
+# recept_list = []
+# for _ in range(int(input())):
+#     censure = input()
+#     if "соль" not in censure:
+#         recept_list.append(censure)
+# print(*recept_list, sep=", ")
+
+# 10
+# result = 0
+# for value in range(50, 101):
+#     result += value ** 3
+# print(result)
+
+# 11
+#  слова длиннее 10 заменить
+# result_list = []
+# for _ in range(int(input())):
+#     censure = input()
+#     if len(censure) > 10:
+#         result_list.append(censure[0] + str(len(censure) - 2) + censure[-1])
+#         pass
+#     else:
+#         result_list.append(censure)
+# print(*result_list, sep="\n")
+#  короткое, но малопонятное решение. цикл интересен: for w in [input() for _ in range(int(input()))] можно использовать
+# # print("\n".join((w, f'{w[0]}{len(w) - 2}{w[-1]}')[len(w) > 10] for w in [input() for _ in range(int(input()))]))
+
 
