e304b98bfe919536631f5a63bbeb548314cd10e6
vgalinskii <vgalinskii@turbosolution.ru>
2022-04-04 09:45:04 +0300

    доработано на работе.

--- Indi_egoroff_channel/list_comprehension.py
+++ Indi_egoroff_channel/list_comprehension.py
@@ -18,19 +18,19 @@
 # print(my_list)
 #
 # 4
-orig_str = list(input())
-bad_str = list(input())
-find_char = ""
-for ch in bad_str:
-    if ch not in orig_str:
-        find_char = ch
-        break
-
-if find_char == "":
-    for index in range(len(orig_str)):
-        if orig_str[index] != bad_str[index]:
-            find_char = bad_str[index]
-            break
-print(find_char if find_char != "" else bad_str[-1])
-# print(orig_str)
-# print(bad_str)
+# orig_str = list(input())
+# bad_str = list(input())
+# find_char = ""
+# for ch in bad_str:
+#     if ch not in orig_str:
+#         find_char = ch
+#         break
+#
+# if find_char == "":
+#     for index in range(len(orig_str)):
+#         if orig_str[index] != bad_str[index]:
+#             find_char = bad_str[index]
+#             break
+# print(find_char if find_char != "" else bad_str[-1])
+# # print(orig_str)
+# # print(bad_str)
