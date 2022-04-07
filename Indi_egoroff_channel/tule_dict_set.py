86146ab18668156da502c0067890157e5d4cb16f
vovchikSev <vovchik@programist.ru>
2022-04-07 21:34:59 +0300

    from home

--- Indi_egoroff_channel/tule_dict_set.py
+++ Indi_egoroff_channel/tule_dict_set.py
@@ -95,17 +95,30 @@
 #
 # 5
 
-morze = {'a': '•—', 'b': '—•••', 'c': '—•—•', 'd': '—••',
-         'e': '•', 'f': '••—•', 'g': '——•', 'h': '••••',
-         'i': '••', 'j': '•———', 'k': '—•—', 'l': '•—••',
-         'm': '——', 'n': '—•', 'o': '———', 'p': '•——•',
-         'q': '——•—', 'r': '•—•', 's': '•••', 't': '—',
-         'u': '••—', 'v': '•••—', 'w': '•——', 'x': '—••—',
-         'y': '—•——', 'z': '——••'}
-in_str = "Houston we have a problem".lower() # input().lower()
-# out_list = []
-for word in in_str.split():
-    # print(word)
-    # out_list.append(" ".join([morze[val] for val in word]))
-    print(" ".join([morze[val] for val in word]))
-# print(*out_list, sep="\n")
+# morze = {'a': '•—', 'b': '—•••', 'c': '—•—•', 'd': '—••',
+#          'e': '•', 'f': '••—•', 'g': '——•', 'h': '••••',
+#          'i': '••', 'j': '•———', 'k': '—•—', 'l': '•—••',
+#          'm': '——', 'n': '—•', 'o': '———', 'p': '•——•',
+#          'q': '——•—', 'r': '•—•', 's': '•••', 't': '—',
+#          'u': '••—', 'v': '•••—', 'w': '•——', 'x': '—••—',
+#          'y': '—•——', 'z': '——••'}
+# in_str = "Houston we have a problem".lower() # input().lower()
+# # out_list = []
+# for word in in_str.split():
+#     # print(word)
+#     # out_list.append(" ".join([morze[val] for val in word]))
+#     print(" ".join([morze[val] for val in word]))
+# # print(*out_list, sep="\n")
+
+# 6.4 кортежи
+my_tuple = (-214, 181, -139, 448, -664, -66, 213, 832, 717, -462, -924, -706, -85, -244, -222, -340, -482, -518, -781, 759, -593, 905, -354, -377, -141, -742, 383, -381, 109, -639, -480, -810, -686, 892, -612, 696, 993, 791, 631, -493, -218, -829, -275, 619, -628, -241, -565, -835, -69, 747, 711, -252, -811, -407, -153, 904, 933, -254, 307, -493, -419, -109, -543, 155, -127, 613, -452, -459, 856, 562, 333, -66, -77, -598, -779, -278, 867, 321, -20, -415, -357, 735, -906, -14, -370, 453, -630, -736, -830, -917, 32, 422, -895, 198, 284, 472, -986, -964, -73, 29)
+# 3
+# print(min(my_tuple), max(my_tuple))
+# 4
+# my_sum = [val for val in my_tuple if bool(val % 2)]
+# print(sum(my_sum) / len(my_sum))
+# 4
+# print(tuple([i for i in range(int(input()), int(input())+1)]))
+# 5
+in_value = int(input())
+print(tuple([i for i in range(in_value, (in_value ** 2) + 1)]))
