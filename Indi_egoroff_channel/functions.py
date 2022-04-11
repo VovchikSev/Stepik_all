9d27a44601cdadf0d8bf283e61def0b53801f747
vgalinskii <vgalinskii@turbosolution.ru>
2022-04-11 09:00:21 +0300

    доработано на работе.

---
+++ Indi_egoroff_channel/functions.py
@@ -0,0 +1,6 @@
+def summa_n(n: int):
+    # return sum([value for value in range(1, n + 1)])
+    print(f"Я знаю, что сумма чисел от 1 до {n} равна {sum([value for value in range(1, n + 1)])}")
+
+summa_n(int(input()))
+# print(f"Я знаю, что сумма чисел от 1 до {in_value} равна {summa_n(in_value)}")
