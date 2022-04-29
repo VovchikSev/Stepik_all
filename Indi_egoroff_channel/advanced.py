de2408b3e7c0823ad69a7ea9b657097cf9debd69
vgalinskii <vgalinskii@turbosolution.ru>
2022-04-29 11:04:12 +0300

    доработано на работе.

---
+++ Indi_egoroff_channel/advanced.py
@@ -0,0 +1,8 @@
+
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers2 = list(map(lambda val:val*val, numbers))
+print(list(map(lambda val:val*val, numbers)))
+print(list(map(lambda val:val**3, numbers)))
