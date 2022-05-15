fe2bfa1b3732a0b95eb9fa17268b91756be4f82f
vovchikSev <vovchik@programist.ru>
2022-05-15 22:45:21 +0300

    from home

--- python_basik_using/basik_princip.py
+++ python_basik_using/basik_princip.py
@@ -1,3 +1,4 @@
+
 # https://stepik.org/lesson/24460/step/10?unit=6766
 """
 9
@@ -16,87 +17,73 @@ global
 None
 bar
 foo
-
-add global a
-create foo global
-add foo b
-
-get foo a
-
-get foo c
-create bar foo
-add bar a
-get bar a
-get bar b
-
-{
-    "global": ["", set(a)]
-    "foo": ["global", set(b)] смотреть есть ли в текущем словаре, 
-    искомое значение. если нет ммотрим выше пока не стнет родитель = ""
-{
-  'global': ['', {'a'}],
-  'foo': ['global', {'b'}],
-  'bar': ['foo', {'a'}]}
-}
-
 """
-in_count = int(input())
-my_dict = {"global": ["", set()]}
+# in_count = int(input())
+# my_dict = {"global": ["", set()]}
+#
+#
+# def get_value(ns: str, narg: str) -> str:
+#     result = None
+#     # проверить есть ли в my_dict[ns]
+#     if ns in my_dict.keys():
+#         parent, values_set = my_dict[ns]
+#         if narg in values_set:
+#             result = ns  # искомое имя в текущем пространстве.
+#         else:
+#             while parent != "":
+#                 if parent in my_dict.keys():
+#                     parent_n, values_set = my_dict[parent]
+#                     if narg in values_set:
+#                         result = parent
+#                         break
+#                     else:
+#                         parent = parent_n
+#     return result
+#
+#
+# for _ in range(in_count):
+#     cmd, name_sp, arg = input().split()
+#     if cmd == "add":
+#         # добавить переменную в namespace
+#         tm_list = my_dict[name_sp]
+#         tm_list[1].add(arg)
+#         my_dict[name_sp] = tm_list
+#
+#     elif cmd == "create":
+#         # создать namespace в arg
+#         my_dict[name_sp] = [arg, set()]
+#
+#     elif cmd == "get":
+#         # получить переменную arg из name_sp
+#         print(get_value(name_sp, arg))
+#
+# print(my_dict)
+
+# https://stepik.org/lesson/24461/step/9?unit=6767
+class Buffer:
+    def __init__(self):
+        self.buffer = []
 
 
-def get_value(ns: str, narg: str) -> str:
-    result = None
-    # проверить есть ли в my_dict[ns]
-    if ns in my_dict.keys():
-        parent, values_set = my_dict[ns]
-        if narg in values_set:
-            result = ns  # искомое имя в текущем пространстве.
-        else:
-            while parent != "":
-                if parent in my_dict.keys():
-                    parent_n, values_set = my_dict[parent]
-                    if narg in values_set:
-                        result = parent
-                        break
-                    else:
-                        parent = parent_n
-    return result
+    def add(self, *a):
+        self.buffer += a
+        last_index = -5 if len()
+        self.in_list = self.buffer[-1]
+        # добавить следующую часть последовательности
 
 
-for _ in range(in_count):
-    cmd, name_sp, arg = input().split()
-    if cmd == "add":
-        # добавить переменную в namespace
-        tm_list = my_dict[name_sp]
-        tm_list[1].add(arg)
-        my_dict[name_sp] = tm_list
-
-    elif cmd == "create":
-        # создать namespace в arg
-        my_dict[name_sp] = [arg, set()]
-
-    elif cmd == "get":
-        # получить переменную arg из name_sp
-        print(get_value(name_sp, arg))
-
-print(my_dict)
-"""
-{'global': [None, {'a'}],
- 'foo': ['global', {'b'}],
- 'bar': ['foo', {'a'}]}
-
-add global a
-create foo global
-add foo b
-create bar foo
-add bar a
-
-a = 0
-def foo():
-  b = 1
-  def bar():
-    a = 2
+    def get_current_part(self):
+        return self.in_list
+        # вернуть сохраненные в текущий момент элементы последовательности
+        # в порядке, в котором они были добавлены
 
 
-
-"""
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part() # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+buf.get_current_part() # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+buf.get_current_part() # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+buf.get_current_part() # вернуть [1]
