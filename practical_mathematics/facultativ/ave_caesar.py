6a97a95b29e4260fa7bd8051b6ad75ace5b81e2a
vovchikSev <vovchik@programist.ru>
2022-06-16 22:02:10 +0300

    from home

--- practical_mathematics/facultativ/ave_caesar.py
+++ practical_mathematics/facultativ/ave_caesar.py
@@ -95,14 +95,104 @@
 #         print(f"{time.time() - start:>.4f}")
 #         break
 
-from random import shuffle
+# from random import shuffle
+#
+#
+# def disc_generator(alphabet: str) -> str:
+#     alphabet = list(alphabet)
+#     shuffle(alphabet)
+#     return "".join(alphabet)
+#
+#
+# print(disc_generator(input()))
 
 
-def disc_generator(alphabet: str) -> str:
-    alphabet = list(alphabet)
-    shuffle(alphabet)
-    return "".join(alphabet)
+#  Цилиндр Джеферсона - это механический шифровальный аппарат, который состоит из нескольких дисков,
+#  на которых записаны алфавиты (состоящие из одинаковых символов, но в случайном порядке).
+#
+# Для шифрования с его помощью текст разбивают на блоки, длина каждого блока равна числу дисков в механизме.
+#
+# Каждый символ блока шифруется с помощью алгоритма Цезаря с использованием соответствующего диска
+# (т.е. 1й символ с помощью 1 диска, 2й символ с помощью 2 диска и т.д.) с общим сдвигом.
+#
+# Таким образом сдвиг (ключ для шифрования Цезаря) не является секретом для цилиндра Джеферсона -
+# цилиндры легко повернуть вокруг оси и на них видны сразу все комбинации расшифровки, так что даже не требуется
+# брутфорс. Однако, если сами цилиндры (а значит и порядок символов в алфавитах) хранятся в секрете,
+# а так же в секрете порядок следования алфавитов, то такой метод шифрования очень надёжен.
+#
+# Переменная clear_alphabet содержит исходный алфавит
+# Переменная n содержит число
+# Создайте n новых алфавитов и поместите список из них в переменную discs.
+# Создайте функцию jefferson_encryption(text, discs, step, reverse=False), реализующую "цилиндр Джеферсона"
+#
+# text - исходный текст
+# discs - список из словарей (строк)
+# step - сдвиг на которых смещаются символы с помощью алгоритма Цезаря (ключ шифрования для шифра Цезаря)
+# reverse - признак расшифровки, если находится в значении True, это значит, что функцию надо использовать
+# для расшифровки текста, т.к. каждый сдвиг должен быть отрицательным. (по умолчанию False)
+
+# пример ввода
+
+# from random import shuffle
+#
+#
+# def disc_generator(alphabet: str) -> str:
+#     ret_l = list(alphabet)
+#     shuffle(ret_l)
+#     return "".join(ret_l)
+#
+#
+# clear_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# n = 6
+# text = "Some encrypted text"
+# disks = [disc_generator(clear_alphabet) for _ in range(n)]
+#
+#
+# def jefferson_encryption(text, discs, step, reverse=False):
+#     print(clear_alphabet)
+#
+# print(disks)
+
+######################################################
+import random
+#n = int(input())
+#clear_alphabet = input()
+discs = []
+random.seed(42)
+for i in range(n):
+    alph = list(map(str, clear_alphabet))
+    random.shuffle(alph)
+    discs.append(alph)
+d = []
+strok = ""
+for i in discs:
+    for o in i:
+        strok += o
+    d.append(strok)
+    strok = ""
+discs = d
+#print("DISCS:")
+#for i in discs:
+#    print(i)
+#print()
 
 
-print(disc_generator(input()))
-
+def jefferson_encryption(text, discs, step, reverse=False):
+    text = text.upper()
+    new_text = ""
+    for i in text:
+        if i in clear_alphabet:
+            new_text+=i
+    k = 0
+    result = ""
+    for t in new_text:
+        i = discs[k].index(t)
+        if reverse==False:
+            i = (i + step)%len(clear_alphabet)
+        elif reverse==True:
+            i = (i - step)%len(clear_alphabet)
+        result +=discs[k][i]
+        k += 1
+        if k>(n-1):
+            k = 0
+    return result
