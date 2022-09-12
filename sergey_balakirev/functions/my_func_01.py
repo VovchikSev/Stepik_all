ee823422f0bc27bd4e0b9deb3963ac5552e3c018
vgalinskii <vgalinskii@turbosolution.ru>
2022-09-12 09:17:38 +0300

    from work

--- sergey_balakirev/functions/my_func_01.py
+++ sergey_balakirev/functions/my_func_01.py
@@ -4,8 +4,8 @@
 #   print(in_list)
 #   return in_list[0] + in_list[1] > in_list[2]
 
-# def is_even(in_value:int) -> bool:
-#   return not bool( in_value % 2)
+def is_even(in_value:int) -> bool:
+  return  bool( in_value % 2)
 
 # # while (n := int(input())) != 1:
 #     # print(('', f'{n}\n')[is_even(n)], end='')
@@ -15,3 +15,11 @@
 #     break
 #   if is_even(value):
 #     print(value)
+
+
+in_list = list(map(int, input().split()))
+
+print(*list(filter(is_even, in_list)))
+for value in in_list:
+  if is_even(value):
+    print(value)
