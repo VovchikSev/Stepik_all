a05aa95258b1b13a3d7dfec09d37b3c28e70d418
vovchikSev <vovchik@programist.ru>
2022-09-15 20:53:46 +0300

    from home

--- sergey_balakirev/functions/my_func_01.py
+++ sergey_balakirev/functions/my_func_01.py
@@ -4,8 +4,8 @@
 #   print(in_list)
 #   return in_list[0] + in_list[1] > in_list[2]
 
-def is_even(in_value:int) -> bool:
-  return  bool( in_value % 2)
+# def is_even(in_value:int) -> bool:
+#   return  bool( in_value % 2)
 
 # # while (n := int(input())) != 1:
 #     # print(('', f'{n}\n')[is_even(n)], end='')
@@ -38,8 +38,26 @@ def is_even(in_value:int) -> bool:
 #   get_sq = get_sq_quadre  
 
 
-def chck_str_len(in_str:str) ->bool:
-  return len(in_str) > 5
+# def chck_str_len(in_str:str) ->bool:
+#   return len(in_str) > 5
 
 
-print(*[value for value in input().split() if chck_str_len(value)])  
+# print(*[value for value in input().split() if chck_str_len(value)])  
+
+
+# def get_tuple(in_str:str)-> tuple:
+#     return tuple([in_str, len(in_str)])
+
+# in_lst = input().split()
+
+# d = {get_tuple(value)[0]:get_tuple(value)[1] for value in in_lst}
+
+# a = sorted(d, key=lambda x: d[x])
+# print(*a)
+
+
+def foo_mul(max_value:int, min_value: int)->int:
+    return max_value * min_value
+
+in_lst = list(map(int, input().split()))
+print(foo_mul(max(in_lst), min(in_lst)))
