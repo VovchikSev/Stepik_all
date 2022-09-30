d6a719fc088cab14958e14524654935dff3ab573
vgalinskii <vgalinskii@turbosolution.ru>
2022-09-30 21:07:18 +0300

    from work

---
+++ sergey_balakirev/functions/lambda_functions.py
@@ -0,0 +1,19 @@
+
+
+lambda a: -a
+
+lambda: return "hello lambda"
+
+lambda d: (d += 1)
+
+lambda d: d += 1
+
+lambda x, y: x+y
+
+lambda: "hello lambda"
+
+lambda x: x
+
+lambda x: return x
+
+lambda a, b: a, b = b, a
