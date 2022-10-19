fad35504468afe11407fa44e7d9bdc2979d42443
vgalinskii <vgalinskii@turbosolution.ru>
2022-10-19 09:45:10 +0300

    доработано на работе.

--- sergey_balakirev/functions/decorator_arguments.py
+++ sergey_balakirev/functions/decorator_arguments.py
@@ -57,18 +57,78 @@ s = input()
 Sample Input: Декораторы - это классно! -> <div>декораторы - это классно!</div>
 """
 
+# def pre_decorator(tag: str = "h1"):
+#     def in_decorator(func):
+#         def wrapper(message: str) -> str:
+#             return f"<{tag}>{func(message)}</{tag}>"
+#         return wrapper
+#
+#     return in_decorator
+#
+#
+# @pre_decorator(tag="div")
+# def prepare_str(s: str) -> str:
+#     return s.lower()
+#
+# print(prepare_str(input()))
 
-def pre_decorator(tag: str = "h1"):
-    def in_decorator(func):
-        def wrapper(message: str) -> str:
-            return f"<{tag}>{func(message)}</{tag}>"
+
+"""
+Подвиг 3. Объявите функцию, которая принимает строку на кириллице и преобразовывает ее в латиницу, используя следующий 
+словарь для замены русских букв на соответствующее латинское написание:
+t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
+     'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+     'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+     'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+Функция должна возвращать преобразованную строку. Замены делать без учета регистра (исходную строку перевести в нижний 
+регистр - малые буквы).
+Определите декоратор с параметром chars и начальным значением " !?", который данные символы преобразует в символ "-" и, 
+кроме того, все подряд идущие дефисы (например, "--" или "---") приводит к одному дефису. Полученный результат должен 
+возвращаться в виде строки.
+Примените декоратор с аргументом chars="?!:;,. " к функции и вызовите декорированную функцию для введенной строки s:
+s = input()
+Результат отобразите на экране.
+Sample Input: Декораторы - это круто!  Sample Output: dekoratory-eto-kruto-
+"""
+
+t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
+     'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+     'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+     'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+
+
+def pre_decorator(chars=" !?"):
+    def decorator(function):
+        def wrapper(message):
+            result = ""
+            in_str = function(message)
+
+            for char in in_str:
+                if char in chars:
+                    result += '-'
+                else:
+                    result += char
+
+            while "--" in result:
+                result = result.replace("--", "-")
+
+            return result
+
         return wrapper
 
-    return in_decorator
+    return decorator
 
 
-@pre_decorator(tag="div")
-def prepare_str(s: str) -> str:
-    return s.lower()
+@pre_decorator(chars="?!:;,. ")
+def converter(text):
+    text = text.lower()
+    result = ""
+    for char in text:
+        if char in t:
+            result += t[char]
+        else:
+            result += char
+    return result
 
-print(prepare_str(input()))
+
+print(converter(input()))
